f69a0bf9cf29b59feaa9a3f95690e1aac2047d43
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-17 12:15:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
    return array.split(' ')
   elsif array.length == 1
     return array[0]
-  else array.length == 3 
+  else 
     array[-1] = 'and ' + array[-1]
     return array.split(', ')
   end
